3058010db908587b39c8ed5ae8f2de7a36f0c24a
idk <idk@idk.local>
2016-04-24 15:27:55 -0400

    Sun Apr 24 15:27:55 EDT 2016

--- debian.sh
+++ debian.sh
@@ -9,7 +9,8 @@ DEBFOLDER=svirfneblin-conky-hud
 
 DEBVERSION=$(date +%Y%m%d)
 
-cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $TOME
 
 git pull origin master
 
@@ -21,7 +22,7 @@ rm -rf $DEBFOLDERNAME
 mkdir $DEBFOLDERNAME
 
 # Copy your script to the source dir
-cp -R $SOURCEBINPATH/ $DEBFOLDERNAME/
+cp -R $TOME $DEBFOLDERNAME/
 cd $DEBFOLDERNAME
 
 pwd
